b3a932770e6afe5ca196e39f97965f0dbc95412b
Vilen Tambovtsev <xumix@ya.ru>
2026-07-17 12:34:13 +0300

    feat: add bounded FsItem semantics

--- ScannerCore/FsItem.cs
+++ ScannerCore/FsItem.cs
@@ -7,28 +7,56 @@ using System.IO;
 
 namespace ScannerCore
 {
+    public enum FsItemKind
+    {
+        File,
+        Directory,
+        Aggregate
+    }
+
     [DebuggerDisplay("Dir:{IsDir}, {Name}, {Size} bytes")]
     public sealed class FsItem
     {
         public FsItem(string name, long size, bool isDir)
+            : this(name, size, isDir ? FsItemKind.Directory : FsItemKind.File)
+        {
+        }
+
+        private FsItem(string name, long size, FsItemKind kind)
         {
             Name = name;
             Size = size;
-            IsDir = isDir;
+            Kind = kind;
         }
 
         public string Name { get; }
         public long Size { get; set; }
-        public bool IsDir { get; }
+        public FsItemKind Kind { get; }
+        public bool IsDir => Kind == FsItemKind.Directory;
+        public bool IsAggregate => Kind == FsItemKind.Aggregate;
+        public bool HasUnretainedChildren { get; internal set; }
         public FsItem? Parent { get; internal set; }
 
         public List<FsItem>? Items { get; set; }
+
+        public static FsItem CreateAggregate(long size) =>
+            new(string.Empty, size, FsItemKind.Aggregate);
 
         internal void AttachChildren(List<FsItem> children)
         {
             Items = children;
             foreach (var child in children)
                 child.Parent = this;
+        }
+
+        public int CountRetainedNodes()
+        {
+            var count = 1;
+            if (Items is null)
+                return count;
+            foreach (var child in Items)
+                count = checked(count + child.CountRetainedNodes());
+            return count;
         }
 
         public bool TryGetPathFrom(FsItem root, out string path)
